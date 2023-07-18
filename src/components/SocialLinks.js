08dbbc4df1e1e67825e5471413d53aef9847e1d6
lakshitaa1809 <110550701+lakshitaa1809@users.noreply.github.com>
2023-07-18 05:51:03 +0530

    Update SocialLinks.js

--- src/components/SocialLinks.js
+++ src/components/SocialLinks.js
@@ -73,7 +73,7 @@ const SocialLinks = () => {
         rounded-br-md ml-[-100px] hover:ml-[-10px] duration-300 hover:rounded-tr-md  "
         >
           <a
-            href="/saranya_resume_FSD.pdf"
+            href="/FullStack Resume_Saranya.pdf"
             className="flex justify-between items-center w-full  "
             download="Saranya Resume"
             target="-blank"
